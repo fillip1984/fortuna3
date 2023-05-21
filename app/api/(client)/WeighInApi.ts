55c73a5ded83b1d2ebcc4dad0befe90a98dd551d
fillip1984 <fillip1984@gmail.com>
2023-05-21 14:15:23 -0400

    added get weigh in

--- app/api/(client)/WeighInApi.ts
+++ app/api/(client)/WeighInApi.ts
@@ -13,6 +13,11 @@ export async function getWeighIns() {
   return (await result.json()) as WeighIn[];
 }
 
+export async function getWeighIn(id: string) {
+  const result = await fetch(`/api/weighIns/${id}`);
+  return (await result.json()) as WeighIn;
+}
+
 export async function deleteWeighIn(id: string) {
   const result = await fetch(`/api/weighIns/${id}`, {
     method: "DELETE",
